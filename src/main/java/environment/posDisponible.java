ff93ad1151d0b8ca5efda88cafa42170f732a08f
VictorLobatoRios <Victor@192.168.1.74>
2015-04-14 16:33:25 -0500

    Trabajando posicion random

--- src/main/java/environment/posDisponible.java
+++ src/main/java/environment/posDisponible.java
@@ -1,29 +1,76 @@
 package environment;
 
+import jadex.extension.envsupport.math.IVector2;
+import jadex.extension.envsupport.math.Vector2Double;
+
 import java.util.ArrayList;
+import java.util.Random;
 
 public class posDisponible {
 	
-	static ArrayList<Integer>[] coord = new ArrayList[3];
+	static ArrayList<Double>[] coord = new ArrayList[3];
+	static IVector2[] pos = new IVector2[3];
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		coord[0] = new ArrayList<Integer>();
-		coord[1] = new ArrayList<Integer>();
+		coord[0] = new ArrayList<Double>();
+		coord[1] = new ArrayList<Double>();
 		llenarLista();
+		crearVectores();
+		obtenerPosicion(Vector2Double.ZERO);
 		
+	}
+	
+	public posDisponible(){
 		
-
 	}
 
 	public static void llenarLista() {
 		// TODO Auto-generated method stub
 		
-		coord[0].add(123);coord[1].add(163);
-		coord[0].add(23);coord[1].add(23789);
-		
+		coord[0].add(7.0);coord[1].add(1.5);
+		coord[0].add(15.0);coord[1].add(1.5);
+		coord[0].add(21.0);coord[1].add(1.5);
 				
+	}
+	
+	private static void crearVectores() {
+		// TODO Auto-generated method stub
+		
+		for (int i=0;i<coord[0].size();i++){
+			pos[i]= Vector2Double.getVector2(coord[0].get(i),coord[1].get(i));
+			System.out.println(pos[i]);
+		}
+		
+	}
+	
+	public static IVector2 obtenerPosicion(IVector2 distance){
+		
+		
+		IVector2 position = Vector2Double.ZERO;
+		coord[0] = new ArrayList<Double>();
+		coord[1] = new ArrayList<Double>();
+		
+		llenarLista();
+		crearVectores();
+		
+		/*for (int i=0;i<pos.length;i++){
+			position.add(pos[i]);
+			System.out.println("position: "+pos[i]);
+		}*/
+		
+		Random randCoord = new Random();
+		
+		position.add(pos[(int)(randCoord.nextDouble()*pos.length)]);
+		
+		
+		//position.randomX(distance.getX(), position.getX());
+		//position.randomY(distance.getY(), position.getY());
+		
+		System.out.println("position: "+position);
+		
+		return position;
 		
 	}
 
