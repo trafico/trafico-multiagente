588e9e4eb663b76d32f72ab51a894434ffcfbae0
VictorLobatoRios <Victor@192.168.1.74>
2015-04-22 21:00:16 -0500

    mapa 90x90

--- src/main/java/environment/posDisponible.java
+++ src/main/java/environment/posDisponible.java
@@ -12,30 +12,124 @@ public class posDisponible
 {
 	
 	static ArrayList<Double>[] coord = new ArrayList[3];
-	static IVector2[] pos = new IVector2[7];
+	static IVector2[] pos = new IVector2[100];
 
-	public static void main(String[] args) {
+	/*public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
 		coord[0] = new ArrayList<Double>();
 		coord[1] = new ArrayList<Double>();
 		llenarLista();
 		crearVectores();
-		Map a = obtenerPosicion();
-		
-	}
+		//Map a = obtenerPosicion();
+		obtenerPosicion();
+	}*/
 	
 
 	public static void llenarLista() {
 		// TODO Auto-generated method stub
 		
-		coord[0].add(7.0);coord[1].add(1.5);
-		coord[0].add(15.0);coord[1].add(1.5);
-		coord[0].add(21.0);coord[1].add(1.5);
-		coord[0].add(3.0);coord[1].add(11.5);
-		coord[0].add(12.0);coord[1].add(11.5);
-		coord[0].add(19.0);coord[1].add(11.5);
-		coord[0].add(27.0);coord[1].add(11.5);
+		coord[0].add(1.0);coord[1].add(1.0);
+		coord[0].add(11.0);coord[1].add(1.0);
+		coord[0].add(21.0);coord[1].add(1.0);
+		coord[0].add(31.0);coord[1].add(1.0);
+		coord[0].add(41.0);coord[1].add(1.0);
+		coord[0].add(51.0);coord[1].add(1.0);
+		coord[0].add(61.0);coord[1].add(1.0);
+		coord[0].add(71.0);coord[1].add(1.0);
+		coord[0].add(81.0);coord[1].add(1.0);
+		coord[0].add(91.0);coord[1].add(1.0);
+		coord[0].add(1.0);coord[1].add(11.0);
+		coord[0].add(11.0);coord[1].add(11.0);
+		coord[0].add(21.0);coord[1].add(11.0);
+		coord[0].add(31.0);coord[1].add(11.0);
+		coord[0].add(41.0);coord[1].add(11.0);
+		coord[0].add(51.0);coord[1].add(11.0);
+		coord[0].add(61.0);coord[1].add(11.0);
+		coord[0].add(71.0);coord[1].add(11.0);
+		coord[0].add(81.0);coord[1].add(11.0);
+		coord[0].add(91.0);coord[1].add(11.0);
+		coord[0].add(1.0);coord[1].add(21.0);
+		coord[0].add(11.0);coord[1].add(21.0);
+		coord[0].add(21.0);coord[1].add(21.0);
+		coord[0].add(31.0);coord[1].add(21.0);
+		coord[0].add(41.0);coord[1].add(21.0);
+		coord[0].add(51.0);coord[1].add(21.0);
+		coord[0].add(61.0);coord[1].add(21.0);
+		coord[0].add(71.0);coord[1].add(21.0);
+		coord[0].add(81.0);coord[1].add(21.0);
+		coord[0].add(91.0);coord[1].add(21.0);
+		coord[0].add(1.0);coord[1].add(21.0);
+		coord[0].add(11.0);coord[1].add(31.0);
+		coord[0].add(21.0);coord[1].add(31.0);
+		coord[0].add(31.0);coord[1].add(31.0);
+		coord[0].add(41.0);coord[1].add(31.0);
+		coord[0].add(51.0);coord[1].add(31.0);
+		coord[0].add(61.0);coord[1].add(31.0);
+		coord[0].add(71.0);coord[1].add(31.0);
+		coord[0].add(81.0);coord[1].add(31.0);
+		coord[0].add(91.0);coord[1].add(31.0);
+		coord[0].add(1.0);coord[1].add(41.0);
+		coord[0].add(11.0);coord[1].add(41.0);
+		coord[0].add(21.0);coord[1].add(41.0);
+		coord[0].add(31.0);coord[1].add(41.0);
+		coord[0].add(41.0);coord[1].add(41.0);
+		coord[0].add(51.0);coord[1].add(41.0);
+		coord[0].add(61.0);coord[1].add(41.0);
+		coord[0].add(71.0);coord[1].add(41.0);
+		coord[0].add(81.0);coord[1].add(41.0);
+		coord[0].add(91.0);coord[1].add(41.0);
+		coord[0].add(1.0);coord[1].add(51.0);
+		coord[0].add(11.0);coord[1].add(51.0);
+		coord[0].add(21.0);coord[1].add(51.0);
+		coord[0].add(31.0);coord[1].add(51.0);
+		coord[0].add(41.0);coord[1].add(51.0);
+		coord[0].add(51.0);coord[1].add(51.0);
+		coord[0].add(61.0);coord[1].add(51.0);
+		coord[0].add(71.0);coord[1].add(51.0);
+		coord[0].add(81.0);coord[1].add(51.0);
+		coord[0].add(91.0);coord[1].add(51.0);
+		coord[0].add(1.0);coord[1].add(61.0);
+		coord[0].add(11.0);coord[1].add(61.0);
+		coord[0].add(21.0);coord[1].add(61.0);
+		coord[0].add(31.0);coord[1].add(61.0);
+		coord[0].add(41.0);coord[1].add(61.0);
+		coord[0].add(51.0);coord[1].add(61.0);
+		coord[0].add(61.0);coord[1].add(61.0);
+		coord[0].add(71.0);coord[1].add(61.0);
+		coord[0].add(81.0);coord[1].add(61.0);
+		coord[0].add(91.0);coord[1].add(61.0);
+		coord[0].add(1.0);coord[1].add(71.0);
+		coord[0].add(11.0);coord[1].add(71.0);
+		coord[0].add(21.0);coord[1].add(71.0);
+		coord[0].add(31.0);coord[1].add(71.0);
+		coord[0].add(41.0);coord[1].add(71.0);
+		coord[0].add(51.0);coord[1].add(71.0);
+		coord[0].add(61.0);coord[1].add(71.0);
+		coord[0].add(71.0);coord[1].add(71.0);
+		coord[0].add(81.0);coord[1].add(71.0);
+		coord[0].add(91.0);coord[1].add(71.0);
+		coord[0].add(1.0);coord[1].add(81.0);
+		coord[0].add(11.0);coord[1].add(81.0);
+		coord[0].add(21.0);coord[1].add(81.0);
+		coord[0].add(31.0);coord[1].add(81.0);
+		coord[0].add(41.0);coord[1].add(81.0);
+		coord[0].add(51.0);coord[1].add(81.0);
+		coord[0].add(61.0);coord[1].add(81.0);
+		coord[0].add(71.0);coord[1].add(81.0);
+		coord[0].add(81.0);coord[1].add(81.0);
+		coord[0].add(91.0);coord[1].add(81.0);
+		coord[0].add(1.0);coord[1].add(91.0);
+		coord[0].add(11.0);coord[1].add(91.0);
+		coord[0].add(21.0);coord[1].add(91.0);
+		coord[0].add(31.0);coord[1].add(91.0);
+		coord[0].add(41.0);coord[1].add(91.0);
+		coord[0].add(51.0);coord[1].add(91.0);
+		coord[0].add(61.0);coord[1].add(91.0);
+		coord[0].add(71.0);coord[1].add(91.0);
+		coord[0].add(81.0);coord[1].add(91.0);
+		coord[0].add(91.0);coord[1].add(91.0);
+		
 	}
 	
 	private static void crearVectores() {
@@ -58,10 +152,10 @@ public class posDisponible
 		llenarLista();
 		crearVectores();
 		
-		/*for (int i=0;i<pos.length;i++){
+		for (int i=0;i<pos.length;i++){
 			position.add(pos[i]);
 			System.out.println("position: "+pos[i]);
-		}*/
+		}
 		
 		Random randCoord = new Random();
 		
@@ -73,12 +167,37 @@ public class posDisponible
 		
 		System.out.println("position: "+position);
 		
+		coord[0].clear();
+		coord[1].clear();
+		
 		Map	ret	= new HashMap();
 		
 		ret.put("position", position);
 		
+		
 		return ret;
 		
-	}	
+	}
+	
+	
+/*public static  void obtenerPosicion(){
+		
+		
+		IVector2 position = Vector2Double.ZERO;
+
+		Random randCoord = new Random();
+		
+		position.add(pos[(int)(randCoord.nextDouble()*pos.length)]);
+		
+		
+		//position.randomX(distance.getX(), position.getX());
+		//position.randomY(distance.getY(), position.getY());
+		
+		System.out.println("position: "+position);
+		
+		coord[0].clear();
+		coord[1].clear();		
+		
+	}*/
 
 }
